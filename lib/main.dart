c61e449d1bfbc60eb2b561f36bcd3cb6461bce83
a1265478 <46800897+a1265478@users.noreply.github.com>
2023-03-07 12:23:05 +0800

    Init firebase config
    Init firebase config

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,8 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(),
+    );
   }
 }
